7bea61c24f9738d0c0e9b2bfb78891771db60390
Juan Antonio Fernandez <jfernandez16@nigul.cide.es>
2023-12-05 20:28:49 +0100

    actividades

--- programacion/Programacion/src/Actividad40.java
+++ programacion/Programacion/src/Actividad40.java
@@ -1,5 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Actividad40 {
-    public static void actividad40 () {
-    
+    public static void actividad40() {
+        List<String> listaNombres = new ArrayList<>();
+        listaNombres.add("Mar");
+        listaNombres.add("Marta");
+        listaNombres.add("Maria");
+        listaNombres.add("Pepe");
+        // final String[] nombres = { "Mar", "Marta", "Maria", "Pepe" };
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Escribe el nombre a buscar ");
+        final String nombre = sc.nextLine();
+        boolean encontrado = listaNombres.contains(nombre);
+        if (encontrado) {
+            System.out.println("Has encontrado el nombre " + nombre);
+        } else {
+            System.out.println("No esta este nombre");
+        }
+        sc.close();
     }
 }
--- programacion/Programacion/src/App.java
+++ programacion/Programacion/src/App.java
@@ -1,21 +1,23 @@
 public class App {
-    //public static void main(String[] args) throws Exception {
-        //Ejercicios1.ejercicio1();
-        //Ejercicios2.ejercicio2();
-        //Actividad2.actividad2();
-        //Actividad5.actividad5();
-        //Actividad11.actividad11();
-        //Actividad12.actividad12();
-        //Actividad14.actividad14();
-        //Actividad16.actividad16();
-        //Actividad25.actividad25();
-        //Actividad26.actividad26();
-        //Actividad30.actividad30();
-        //Actividad31.actividad31();
-        //Actividad32.actividad32();
-        //Actividad33.actividad33();
-        //Actividad34.actividad34();
-        //Actividad35.actividad35();
-        //MonkeyIsland.MonkeyIsland();
-    //}
+    public static void main(String[] args) throws Exception {
+        // Ejercicios1.ejercicio1();
+        // Ejercicios2.ejercicio2();
+        // Actividad2.actividad2();
+        // Actividad5.actividad5();
+        // Actividad11.actividad11();
+        // Actividad12.actividad12();
+        // Actividad14.actividad14();
+        // Actividad16.actividad16();
+        // Actividad25.actividad25();
+        // Actividad26.actividad26();
+        // Actividad30.actividad30();
+        // Actividad31.actividad31();
+        // Actividad32.actividad32();
+        // Actividad33.actividad33();
+        // Actividad34.actividad34();
+        // Actividad35.actividad35();
+        // MonkeyIsland.MonkeyIsland();
+        Actividad41.actividad41();
+    }
+
 }

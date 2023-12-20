12e5395b000a526e46664884b948711bc607ecfb
Juan Antonio Fernandez <jfernandez16@nigul.cide.es>
2023-12-20 16:09:33 +0100

    Actividades

--- entornos/JuegoEntornos/elementos.py
+++ entornos/JuegoEntornos/elementos.py
@@ -5,7 +5,8 @@ class Nave(pygame.sprite.Sprite):
     def __init__(self, posicion) -> None:
         super().__init__()
         #Cargamos la imagen
-        self.images = [pygame.image.load("../python/nave1.png"),pygame.image.load("../python/nave2.png")]
+        images = [pygame.image.load("../python/nave1.png"),pygame.image.load("../python/nave2.png")]
+        self.images = [pygame.transform.scale(images[0],(30,60)),pygame.transform.scale(images[1],(30,60))]
         self.indice_images = 0
         self.image = self.images[self.indice_images]
         self.contador_image = 0
@@ -13,17 +14,26 @@ class Nave(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         #Actualizamos la posicion del rectangulo
         self.rect.topleft=posicion
+        self.ultimo_disparo = 0
+        
         
     def update (self,*args: any,**kwargs: any):
         #Capturamos las teclas
         teclas = args[0]
+        #Capturamos grupo_sprite_todos
+        grupo_sprite_todos = args[1]
+        #Capturamos grupo_sprite_balas
+        grupo_sprite_balas = args[2]
         #Capturamos la pantalla
         pantalla = pygame.display.get_surface()
         #Gestionamos las teclas
         if teclas[pygame.K_LEFT]:
             self.rect.x -= 2
             self.rect.x = max(0, self.rect.x)
-            
+        #Aqui disparamos.
+        if teclas[pygame.K_SPACE]:
+            self.disparar(grupo_sprite_todos, grupo_sprite_balas)
+        
         if teclas[pygame.K_RIGHT]:
             self.rect.x +=2
             pantalla = pygame.display.get_surface()
@@ -33,18 +43,62 @@ class Nave(pygame.sprite.Sprite):
         self.indice_images = self.contador_image // 20
         self.image = self.images[self.indice_images]
     
+    def disparar(self, grupo_sprite_todos, grupo_sprite_balas):
+        momento_actual = pygame.time.get_ticks()
+        if momento_actual >  self.ultimo_disparo +200:
+            bala = Bala((self.rect.x + self.image.get_width() /2, self.rect.y))
+            grupo_sprite_todos.add(bala)
+            grupo_sprite_balas.add(bala)
+            self.ultimo_disparo = momento_actual
+        
 class Enemigo(pygame.sprite.Sprite):
     #Aqui ceamos el costructor
     def __init__(self, posicion) -> None:
         super().__init__()
         #Cargamos la imagen
         imagen = pygame.image.load("../python/nave1.png")
-        self.image = pygame.transform.rotate(imagen, 180)
+        self.image = pygame.transform.scale(imagen,(60,30))
+        self.image = pygame.transform.rotate(self.image, 180)
+        self.mask = pygame.mask.from_surface(self.image)
         #Cremamos un rectangulo a partir de la imagen
         self.rect = self.image.get_rect()
         #Actualizamos la posicion del rectangulo
-        self.rect.topleft=posicion
+        self.rect.topleft = posicion
         
     def update (self,*args: any,**kwargs: any) -> None:
         self.rect.y += 1
-    
+        pantalla = pygame.display.get_surface()
+        if (self.rect.y > pantalla.get_height()):
+            self.kill()
+            
+        #Capturamos el argumento 2 -> grupo_sprite_balas
+        grupo_sprite_balas = args[2]
+        bala_colision = pygame.sprite.spritecollideany(self, grupo_sprite_balas, pygame.sprite.collide_mask)
+        if bala_colision:
+            self.kill()
+            bala_colision.kill()
+            
+class Fondo(pygame.sprite.Sprite):
+    def __init__(self, posicion) -> None:
+        super().__init__()
+        images = pygame.image.load("../python/fondo.png")
+        pantalla = pygame.display.get_surface()
+        self.image = pygame.transform.scale(images, (pantalla.get_width(), images.get_height()))
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (0,0)
+        
+class Bala(pygame.sprite.Sprite):
+    def __init__(self, posicion) -> None:
+        super().__init__()
+        image = pygame.image.load("../python/bola-de-fuego.png")
+        self.image = pygame.transform.scale(image,(30,30))
+        #Creamos un rectangulo
+        # self.image = pygame.Surface((5,10))
+        # self.image.fill((255,0,0))
+        self.mask = pygame.mask.from_surface(self.image)
+        self.rect = self.image.get_rect()
+        self.rect.center = posicion
+        
+    def update (self,*args: any,**kwargs: any) -> None:
+        self.rect.y -=5
+        
--- entornos/JuegoEntornos/spaceInvade.py
+++ entornos/JuegoEntornos/spaceInvade.py
@@ -16,20 +16,28 @@ FPS = 60
 running = True
 
 #Creamos la nave
-posicion = (200,200)
+posicion = (360,500)
 nave = elementos.Nave(posicion)
 
 #Creamos un grupo de sprites
-grupo_sprites = pygame.sprite.Group(nave)
-grupo_sprites.add(elementos.Nave((100,100)))
-grupo_sprites.add(elementos.Nave((200,100)))
-grupo_sprites.add(elementos.Nave((300,100)))
+# grupo_sprites = pygame.sprite.Group()
+# grupo_sprites.add(elementos.Fondo())
+# grupo_sprites.add(elementos.Nave((470,100)))
+# # grupo_sprites.add(elementos.Nave((200,100)))
+# # grupo_sprites.add(elementos.Nave((300,100)))
+grupo_sprite_todos = pygame.sprite.Group()
+grupo_sprite_enemigos = pygame.sprite.Group()
+grupo_sprite_balas = pygame.sprite.Group()
 
-enemigo = elementos.Enemigo((50,50))
-grupo_sprites.add(enemigo)
+grupo_sprite_todos.add(elementos.Fondo((0,0)))
+grupo_sprite_todos.add(nave)
+
+
+# enemigo = elementos.Enemigo((50,50))
+# grupo_sprites.add(enemigo)
 #Creamos una variable que almacena la ultima vez que se creo un enemigo
 ultimo_enemigo_creado = 0
-frecuencia_creacion_enemigos = 200
+frecuencia_creacion_enemigos = 2000
 #Creamos el bucle principal
 while running:
     #limitamos el bucle al framerate que hemos definido
@@ -44,17 +52,24 @@ while running:
     momento_actual = pygame.time.get_ticks()
     if (momento_actual > ultimo_enemigo_creado + frecuencia_creacion_enemigos):
         cordX = random.randint(0, pantalla.get_width())
-        cordY = 0
-        grupo_sprites.add(elementos.Enemigo((cordX, cordY)))
+        cordY = -200
+        #Creamos el enemigo y lo añadismos a los grupos.
+        enemigo = elementos.Enemigo((cordX, cordY))
+        grupo_sprite_todos.add(enemigo)
+        grupo_sprite_enemigos.add(enemigo)
+        #Actualizamos el momento del ultimo enemigo creado
         ultimo_enemigo_creado = momento_actual
         
     #Capturamos las teclas
     teclas = pygame.key.get_pressed()
+    # if teclas[pygame.K_SPACE]:
+    #     nave.disparar(grupo_sprite_todos)
+    
     
     #Pintamos
     pantalla.fill((255, 255, 255))
-    grupo_sprites.update(teclas)
-    grupo_sprites.draw(pantalla)
+    grupo_sprite_todos.update(teclas, grupo_sprite_todos, grupo_sprite_balas)
+    grupo_sprite_todos.draw(pantalla)
 
 
     #Redibujar la pantalla
